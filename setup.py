163d6d6f4153f7a60e1e152bdbc05a23f7385789
QMoayedpour <quentin.moayedpour@ensae.fr>
2024-12-14 15:34:50 +0000

    add dependancies

--- setup.py
+++ setup.py
@@ -4,5 +4,16 @@ setup(
     name="ML_fin",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["numpy", "pandas", "tqdm", "matplotlib", "scikit-learn"],
+    extras_require={
+        'scrap': [
+            'bs4',
+            'spacy',
+            'selenium',
+            "pdfplumber",
+            "pdfminer",
+            "urllib3"
+
+        ],
+    },
 )
