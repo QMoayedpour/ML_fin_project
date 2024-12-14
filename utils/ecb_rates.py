75c6c9540ca6aa7d612b2aca58aeb1abc4c3fb13
alexandre <alexandre.viard@ensae.fr>
2024-12-14 14:57:34 +0100

    supprimer fichier inutile

---
+++ utils/ecb_rates.py
@@ -0,0 +1,28 @@
+def get_ecb_rates():
+    data_rates = pd.read_csv(
+
+    'data/ECB Data Portal daily_20241213172734.csv',
+    index_col=0,
+    header=0,
+    names=[
+        'Date',
+        'Time_Period',
+        'Deposit_Change',
+        'Deposit_Level',
+        'Lending_Change',
+        'Lending_Level',
+        'Refinancing_Change',
+        'Fixed_Rate_Level',
+        'Variable_Rate_Level',
+        'Deposit_Level_Alt',
+        'Fixed_Rate_Level_Alt',
+        'Min_Bid_Rate_Level'
+    ],
+    
+    parse_dates=True,
+
+    ).drop(columns='Time_Period')
+
+    return data_rates
+
+
