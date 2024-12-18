3d15fc8fd8cda079314ac13407be81bf941452cf
QMoayedpour <quentin.moayedpour@ensae.fr>
2024-12-18 21:27:55 +0000

    add temp

--- utils/utils.py
+++ utils/utils.py
@@ -2,11 +2,11 @@ import time
 import pandas as pd
 
 
-def get_previous_values(date, n, df2):
+def get_previous_values(date, n, df2, col="content"):
     """Get the content of df2 from the n days before date
     """
     start_date = date - pd.Timedelta(days=n)
-    result = df2[(df2.index >= start_date) & (df2.index <= date)]['content'].tolist()
+    result = df2[(df2.index >= start_date) & (df2.index <= date)][col].tolist()
 
     return result
 
