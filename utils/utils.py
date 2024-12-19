69198b2bd4ccd41d600c2657b4318c7d11f291e5
QMoayedpour <quentin.moayedpour@ensae.fr>
2024-12-19 15:15:29 +0000

    small correction

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ def get_previous_values(date, n, df2, col="content"):
     """Get the content of df2 from the n days before date
     """
     start_date = date - pd.Timedelta(days=n)
-    result = df2[(df2.index >= start_date) & (df2.index <= date)][col].tolist()
+    result = df2[(df2.index >= start_date) & (df2.index < date)][col].tolist()
 
     return result
 
